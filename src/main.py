75b4ceb8fe88de4546adf1363a5d4c7d9bcaec52
JamesHarcourt7 <jh3g20@soton.ac.uk>
2022-07-02 16:03:11 +0100

    Added slow cooker state
    Next is to set up the pi

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import discord
+import gpiozero
 import json
+import time
 
 
 class SlowCookerBot(discord.Client):
@@ -7,6 +9,15 @@ class SlowCookerBot(discord.Client):
     def __init__(self, user_id):
         super().__init__()
         self.user_id = user_id
+        
+        #self.servo = gpiozero.Servo(17)
+        self.off = 15
+        self.low = 30
+        self.medium = 45
+        self.warm = 60
+        
+        self.start_time = 0
+        self.status = "off"
 
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -17,10 +28,42 @@ class SlowCookerBot(discord.Client):
             return
 
         if message.author.id == self.user_id:
-            if message.content == "!start":
-                await message.channel.send("i cant")
-            elif message.content == "!stop":
-                await message.channel.send("i cant")
+            if message.content == "!off":
+                if self.start_time == 0:
+                    await message.channel.send("Slow cooker is already off")
+                else:
+                    await message.channel.send("Turned off. Time elapsed: {}".format(formatTime(time.time() - self.start_time)))
+                    self.start_time = 0
+                    self.status = "off"
+                    
+            elif message.content == "!low":
+                if self.start_time == 0:
+                    await message.channel.send("Turned on. Setting temperature to low")
+                else:
+                    await message.channel.send("Setting temperature to low. Time elapsed: {}".format(formatTime(time.time() - self.start_time)))
+                self.start_time = time.time()
+                self.status = "low"
+                
+            elif message.content == "!medium":
+                if self.start_time == 0:
+                    await message.channel.send("Turned on. Setting temperature to medium")
+                else:
+                    await message.channel.send("Setting temperature to medium. Time elapsed: {}".format(formatTime(time.time() - self.start_time)))
+                self.start_time = time.time()
+                self.status = "medium"
+                
+            elif message.content == "!warm":
+                if self.start_time == 0:
+                    await message.channel.send("Turned on. Set to keep warm.")
+                else:
+                    await message.channel.send("Set to keep warm. Time elapsed: {}".format(formatTime(time.time() - self.start_time)))
+                self.start_time = time.time()
+                self.status = "warm"
+                
+            elif message.content == "!status":
+                await message.channel.send("Current status: {}".format(self.status)) # self.servo.value))
+                await message.channel.send("Time elapsed: {}".format(time.time() - self.start_time))
+                
         else:
             await message.channel.send("You do not have slowcooker privileges.")
 
@@ -31,9 +74,15 @@ def getConfig():
     f.close()
     return config
 
+def formatTime(diff):
+    struct = time.gmtime(diff)
+    return "{}hrs {}mins {}secs".format(struct.tm_hour, struct.tm_min, struct.tm_sec)
+
+def main():
+    config = getConfig()
+    bot = SlowCookerBot(config['user'])
+    bot.run(config['token'])
+
 
 if __name__ == "__main__":
-    config = getConfig()
-
-    client = SlowCookerBot(config["user"])
-    client.run(config["token"])
+    main()
